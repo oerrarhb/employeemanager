7b02f3f213df99f2b9cee4e0d71295d6138dd6af
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-03-08 14:38:01 +0100

    add controller layer

--- src/main/java/com/othmane/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/othmane/employeemanager/EmployeemanagerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeemanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeemanagerApplication.class, args);
+    }
 
 }
